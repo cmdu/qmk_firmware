1c90cd816a37d4d95bb0882c6c13b075f6522796
cmdu <tobias@2551.cc>
2020-01-28 21:14:11 +0100

    revamped keymap

--- keyboards/tada68/keymaps/cmdu/config.h
+++ keyboards/tada68/keymaps/cmdu/config.h
@@ -2,4 +2,4 @@
 
 #define LSPO_KEY KC_8
 #define RSPC_KEY KC_9
-
+#define TAPPING_TERM 200
--- keyboards/tada68/keymaps/cmdu/keymap.c
+++ keyboards/tada68/keymaps/cmdu/keymap.c
@@ -18,41 +18,41 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
+   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Del |
    * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Slp |
    * |----------------------------------------------------------------|
-   * |MT-Hy-Rig|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;| '|Return|PgUp|
+   * |MT-LCtrl-Esc| A| S| D|  F|  G|  H|  J|  K|  L|  ;| '|Return|Mute|
    * |----------------------------------------------------------------|
-   * |S-PO   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|S-PC   | Up|PgDn|
+   * |LS-PO   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|RS-PC  |BUp| V+|
    * |----------------------------------------------------------------|
-   * |LCtrl|Alt|Cmd |        Space          |Alt| FN|RCtrl|Lef|Dow|Rig|
+   * |Hyper|Alt|Cmd|           Space           |Alt|FN|RWD|FFW|BDn| V-|
    * `----------------------------------------------------------------'
    */
 [_BL] = LAYOUT_ansi(
-  KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,   KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,  KC_BSLS,   KC_DEL, \
-  MT(MOD_HYPR, KC_RIGHT), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_ENT, KC_PGUP, \
-  KC_LSPO,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSPC,KC_UP,  KC_PGDN, \
-  KC_LCTRL, KC_LALT, KC_LGUI,                KC_SPC,                        KC_RALT,MO(_FL), KC_RCTRL, KC_LEFT, KC_DOWN,   KC_RGHT),
+  KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,   KC_DEL, \
+  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,  KC_BSLS,   KC_SLEP, \
+  MT(KC_LCTRL, KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, KC_ENT, 	KC__MUTE, \
+  KC_LSPO,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH, KC_RSPC, KC_BRIU,	KC__VOLUP, \
+  MOD_HYPR, KC_LALT, KC_LGUI,       			   KC_SPC,    			     KC_RALT, MO(_FL), KC_MRWD, KC_MFFD, KC_BRID, 	KC__VOLDOWN),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
-   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |    |
    * |----------------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |   |   |   |   |   |     |Hme |
+   * |     |   |Up |   |   |   |   |   |   |   |   |   |   |     |    |
    * |----------------------------------------------------------------|
-   * |      |<- |Dn | ->|   |   |   |   |   |   |   |   |        |End |
+   * |      |<- |Dn | ->|   |   |<- |Dn |Up | ->|   |   |        |    |
    * |----------------------------------------------------------------|
-   * |       |Bl-br|Bl-st|Bl-|Bl|Bl+|   |V-|V+|MUT|   |   McL|MsU|McR |
+   * |       |Bl-br|Bl-st|Bl-|Bl|Bl+|   |  |  |   |   |      |   |    |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
+   * |    |    |    |                       |   |   |    |   |   |    |
    * `----------------------------------------------------------------'
    */
 [_FL] = LAYOUT_ansi(
-  _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
-  _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______, _______,KC_HOME, \
-  _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______,_______,_______,_______,_______,        _______,KC_END, \
-  _______,BL_BRTG,BL_STEP,BL_DEC, BL_TOGG,BL_INC, _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
-  _______,_______,_______,                 _______,               _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
+  _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,_______,  \
+  _______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+  _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,_______,_______, \
+  _______,BL_BRTG,BL_STEP,BL_DEC, BL_TOGG,BL_INC, _______,_______,_______,_______,_______,_______,_______,_______, \
+  _______,_______,_______,                 _______,               _______,_______,_______,_______,_______,_______),
 };
